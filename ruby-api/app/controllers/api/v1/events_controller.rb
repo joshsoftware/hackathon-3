614dfe424bb6b2285bd0d8006056241991a6ef2c
Shubhh <shubham.kumar@joshsoftware.com>
2024-12-20 23:28:09 +0530

    create event create API

---
+++ ruby-api/app/controllers/api/v1/events_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class EventsController < ApplicationController
+      def create
+        byebug
+        events_data = params[:clicks] # Assuming the JSON body contains "clicks" key as shown in the example
+        service = EventsService::Create.new(events_data)
+    
+        if service.call
+          render json: { message: "Events created successfully" }, status: :created
+        else
+          render json: { error: "Failed to create events" }, status: :unprocessable_entity
+        end
+      end
+    end
+  end
+end
