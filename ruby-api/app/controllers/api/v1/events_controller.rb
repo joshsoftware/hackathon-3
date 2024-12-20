8e6c6b881381b73de5fb6642ec1dd9d1cff0a134
Shubhh <shubham.kumar@joshsoftware.com>
2024-12-21 00:30:54 +0530

    optimised code for controllers and services

--- ruby-api/app/controllers/api/v1/events_controller.rb
+++ ruby-api/app/controllers/api/v1/events_controller.rb
@@ -2,13 +2,27 @@ module Api
   module V1
     class EventsController < ApplicationController
       def create
-        events_data = params[:clicks] # Assuming the JSON body contains "clicks" key as shown in the example
+        events_data = create_params # Assuming the JSON body contains "clicks" key as shown in the example
         service = EventsService::Create.new(events_data)
     
         if service.call
           render json: { message: "Events created successfully" }, status: :created
         else
           render json: { error: "Failed to create events" }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def create_params
+        params.require(:clicks).map do |click|
+          click.permit(
+            :uid,
+            :element_id,
+            :now,
+            :action,
+            :url
+          )
         end
       end
     end
--- ruby-api/app/services/events_service/create.rb
+++ ruby-api/app/services/events_service/create.rb
@@ -6,20 +6,16 @@ module EventsService
   
     def call
       @events_params.each do |event_param|
-        create_event(event_param)
+        Event.create(transformed_params(event_param))
       end
     end
-  
+
     private
-  
-    def create_event(event_param)
-      Event.create(
-        uid: event_param["unique_identifier"],
-        action: event_param["action"],
-        action_performed_at: Time.at(event_param["now"] / 1000.0),
-        url: event_param["url"],
-        element_id: event_param["element_identifier"]
-      )
+
+    def transformed_params(event_param)
+      event_param.merge(
+        "action_performed_at" => Time.at(event_param["now"] / 1000.0)
+      ).except("now") # Remove "now" key after transformation
     end
   end
 end
