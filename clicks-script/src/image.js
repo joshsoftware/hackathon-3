1e285c0f620709637c8f80e08695e1ac563ea6cc
Suhaan Bhandary <suhaan.bhandary@joshsoftware.com>
2024-12-21 01:25:22 +0530

    added image logic and added vite

---
+++ clicks-script/src/image.js
@@ -0,0 +1,12 @@
+const captureImage = async () => {
+  const image = await html2canvas(document.body).then(function (canvas) {
+    let img = canvas.toDataURL("image/png");
+
+    let screenshotImage = new Image();
+    screenshotImage.src = img;
+
+    return img;
+  });
+
+  return image;
+};
