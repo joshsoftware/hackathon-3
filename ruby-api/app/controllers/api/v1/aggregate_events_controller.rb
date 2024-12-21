e8382488ee04a6cad99d1fb66a1eecc231ce097c
Shubhh <shubham.kumar@joshsoftware.com>
2024-12-21 09:27:13 +0530

    controller code added for updating aggregate events

--- ruby-api/app/controllers/api/v1/aggregate_events_controller.rb
+++ ruby-api/app/controllers/api/v1/aggregate_events_controller.rb
@@ -1,14 +1,33 @@
-class AggregateEventsController < ApplicationController
+module Api
+  module V1
+    class AggregateEventsController < ApplicationController
+      # GET /aggregate_events
   
-  # GET /aggregate_events
-
-  def index
-    grouped_events = AggregateEvent.joins(:action, :user_event)
-                                   .group(:action_id)
-                                   .select(
-                                     'aggregate_events.action_id, actions.action, actions.url, actions.element, user_events.user_agent, COUNT(aggregate_events.id) as rage_count'
-                                   )
+      def index
+        grouped_events = AggregateEvent.joins(:action, :user_event)
+                                      .group(:action_id)
+                                      .select(
+                                        'aggregate_events.action_id, actions.action, actions.url, actions.element, user_events.user_agent, COUNT(aggregate_events.id) as rage_count'
+                                      )
+      
+        render json: grouped_events
+      end
   
-    render json: grouped_events
+      def update_all
+        params[:ids].each do |id|
+          AggregateEvent.find(id).update(update_params)
+        end
+  
+        render json: {
+          message: "Updated successfully"
+        }, status: :ok
+      end
+  
+      private
+  
+      def update_params
+        params.permit(:screenshot)
+      end
+    end
   end
 end
